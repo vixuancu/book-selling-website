f100c8da5c793111568af96c529dd2404254d6d2
vixuancu <vixuancu2004@gmail.com>
2024-12-29 21:37:15 +0700

    xử lí lỗi theo trình tự refresh token

--- src/services/axios.customize.ts
+++ src/services/axios.customize.ts
@@ -1,12 +1,20 @@
 import axios from "axios";
-
+import { Mutex } from "async-mutex";
+const mutex = new Mutex();
 const createInstanceAxios = (baseURL: string) => {
   const instance = axios.create({
     // baseURL: import.meta.env.VITE_BACKEND_URL,
     baseURL: baseURL,
-    withCredentials: true,
+    withCredentials: true, // truyền cookie vào header
   });
-
+  const handleRefreshToken = async () => {
+    return await mutex.runExclusive(async () => {
+      // cookie tự động được gửi theo header
+      const res = await instance.get("/api/v1/auth/refresh");
+      if (res && res.data) return res.data.access_token;
+      else return null;
+    });
+  };
   // Add a request interceptor
   instance.interceptors.request.use(
     function (config) {
@@ -33,9 +41,17 @@ const createInstanceAxios = (baseURL: string) => {
       }
       return response;
     },
-    function (error) {
+    async function (error) {
       // Any status codes that falls outside the range of 2xx cause this function to trigger
       // Do something with response error
+      if (error.config && error.response && +error.response.status === 401) {
+        const access_token = await handleRefreshToken();
+        if (access_token) {
+          error.config.headers["Authorization"] = `Bearer ${access_token}`;
+          localStorage.setItem("access_token", access_token);
+          return instance.request(error.config); // tự động gọi lại khi có access_token mới
+        }
+      }
       if (error && error.response && error.response.data) {
         return error.response.data;
       }
